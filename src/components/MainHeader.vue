612704521b87c92ff11729fa13289c2107c3963b
spineag2 <spineag@gmail.com>
2023-04-28 16:10:24 +0300

    adds

---
+++ src/components/MainHeader.vue
@@ -0,0 +1,12 @@
+<script setup>
+</script>
+
+<template>
+    <div>
+        some header here
+    </div>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/views/MainPageView.vue
@@ -0,0 +1,17 @@
+<script setup>
+import MainHeader from '../components/MainHeader.vue'
+</script>
+
+<template>
+  <header>
+    <MainHeader />
+  </header>
+
+  <main>
+    asd
+  </main>
+
+  <footer>
+
+  </footer>
+</template>
