9eb972ccf89820e6129bd564910795f873a0a659
spineag2 <spineag@gmail.com>
2023-05-31 16:32:24 +0300

    news items

--- src/components/homepage/NewsScreenItem.vue
+++ src/components/homepage/NewsScreenItem.vue
@@ -4,13 +4,20 @@
 
 <template>
     <div class="news_item">
-       as
+       <div class="news_item_img" style="background-image: url('src/assets/img/photo_bikers.jpg');"></div>
     </div>
 </template>
 
 <style scoped>
     .news_item{
-        max-width: 285px;
-        
+        /* max-width: 285px; */
+        width:100%;
+    }
+    .news_item_img{
+        width:100%;
+        height:180px;
+        background-repeat: no-repeat;
+        background-position: center;
+        background-size: cover;
     }
 </style>
