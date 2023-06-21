b2aa5461b94f09d264b9bf7854390033b094d422
spineag2 <spineag@gmail.com>
2023-06-21 16:29:42 +0300

    help screen 2

--- src/components/homepage/HelpScreenItem.vue
+++ src/components/homepage/HelpScreenItem.vue
@@ -3,72 +3,96 @@
 </script>
 
 <template>
-    <div class="news_item">
-       <div class="news_item_img" style="background-image: url('src/assets/img/photo_bikers.jpg');"> </div>
-        <div class="news_item_texts">
-            <div class="news_item_type bg-emerald-600">
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="inherit" class="w-6 h-6">
-                <path stroke-linecap="round" stroke-linejoin="round" d="M12 7.5h1.5m-1.5 3h1.5m-7.5 3h7.5m-7.5 3h7.5m3-9h3.375c.621 0 1.125.504 1.125 1.125V18a2.25 2.25 0 01-2.25 2.25M16.5 
-                7.5V18a2.25 2.25 0 002.25 2.25M16.5 7.5V4.875c0-.621-.504-1.125-1.125-1.125H4.125C3.504 3.75 3 4.254 3 4.875V18a2.25 2.25 0 002.25 2.25h13.5M6 7.5h3v3H6v-3z" /> </svg>
-            </div>
-            <div class="news_item_date text-gray-400">12.05.23</div>
-            <div class="news_item_title">Фрукти - важлива частина здорового харчування</div>
-            <div class="news_item_link">
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="inherit" class="w-6 h-6 velo_stroke_gray">
-                <path stroke-linecap="round" stroke-linejoin="round" d="M17.25 8.25L21 12m0 0l-3.75 3.75M21 12H3" /> </svg>
+    <div class="help_item">
+        <div class="help_cont">
+            <div class="help_title">Обслуговування</div>
+            <ul>
+                <li>Передні та задні передачі</li>
+                <li>Чистка та смазка цепу</li>
+                <li>Гідравлічні тормоза</li>
+                <li>Механічні тормоза</li>
+                <li>Покраска сідла</li>
+            </ul>
+            <div class="help_avatar">
+                <img src="../../assets/img/ava_small.jpg" alt=""/>
             </div>
         </div>
+        <div class="help_overlay">
+            <div class="help_overlay_img" style="background-image: url('src/assets/img/repair_man1.png');"> </div>
+            <div class="help_overlay_line">
+                <span>099 999 99 99</span>
+                <span>Володимирко</span>
+            </div>
+        </div>
+       
         
     </div>
 </template>
 
 <style scoped>
-    .news_item{
-        /* max-width: 285px; */
-        width:100%;
-        height:100%;
-        cursor: pointer;
-        box-shadow: 2px 2px 3px rgba(0, 0, 0, 0.1);
-    }
-    .news_item:hover{
-        box-shadow: 3px 3px 3px rgba(0, 0, 0, 0.2);
-    }
-    .news_item_img{
-        width:100%;
-        height:180px;
-        background-repeat: no-repeat;
-        background-position: center;
-        background-size: cover;
-    }
-    .news_item_texts{
-        position: relative;
-        min-height: 100px;
-        background-color: white;
-        padding: 10px 20px;
-    }
-    .news_item_type{
-        width: 40px;
-        height: 40px;
-        position: absolute;
-        top: -20px;
-        left: 20px;
-        display: flex;
-        align-items: center;
-        justify-content: center;
-    }
-    .news_item_type svg{
-        stroke: white;
-    }
-    .news_item_date{
-        text-align: right;
-        font-size: 12px;
-    }
-    .news_item_title{
-
-    }
-    .news_item_link{
-        position:absolute;
-        bottom:10px;
-        right:20px;
-    }
+.help_item{
+    width:100%;
+    height:220px;
+    cursor: pointer;
+    box-shadow: 2px 2px 3px rgba(0, 0, 0, 0.1);
+    background-color: white;
+    display: block;
+}
+.help_cont{
+    width: 100%;
+    height: 100%;
+    padding: 20px;
+}
+.help_title{
+    font-weight: bold;
+    font-size: 18px;
+}
+.help_cont ul{
+    list-style-type: square;
+    list-style-position: outside;
+}
+.help_cont li{
+    margin-left: 15px;
+}
+.help_avatar{
+    position: absolute;
+    bottom: 20px;
+    right: 20px;
+}
+.help_avatar img{
+    width: 60px;
+    height: 60px;
+    object-fit: cover;
+    border-radius: 50%;
+}
+.help_overlay{
+    position: absolute;
+    top: 0;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    height: 100%;
+    width: 100%;
+    opacity: 0;
+    transition: .1s ease;
+}
+.help_overlay_img{
+    width: 100%;
+    height: 190px;
+    background-position: center;
+    background-repeat: no-repeat;
+    background-size: cover;
+}
+.help_overlay_line{
+    height:30px;
+    width:100%;
+    font-size: 18px;
+    display: flex;
+    align-items: center;
+    justify-content: space-evenly;
+    background-color: white;
+}
+.help_item:hover .help_overlay {
+  opacity: 1;
+}
 </style>
