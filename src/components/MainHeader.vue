e256ca84a6c72ac19c6db31636facc040026d6f6
spineag2 <spineag@gmail.com>
2023-07-04 11:53:17 +0300

    header hover effects

--- src/components/MainHeader.vue
+++ src/components/MainHeader.vue
@@ -5,11 +5,11 @@
     <div class="w-full flex items-center header_container">
         <nav class="velo_container flex justify-between items-center">
             <RouterLink to="/" class="mr-20"> <img class="logo_header" src = "../../src/assets/svg/logo.svg" alt=""/> </RouterLink>
-            <RouterLink to="/news">Новини</RouterLink>
-            <RouterLink to="/projects">Проекти</RouterLink>
-            <RouterLink to="/routes">Маршрути</RouterLink>
-            <RouterLink to="/about">Веловараш</RouterLink>
-            <RouterLink to="/help">Допомога</RouterLink>
+            <RouterLink to="/news"><span>Новини</span></RouterLink>
+            <RouterLink to="/projects"><span>Проекти</span></RouterLink>
+            <RouterLink to="/routes"><span>Маршрути</span></RouterLink>
+            <RouterLink to="/about"><span>Веловараш</span></RouterLink>
+            <RouterLink to="/help"><span>Допомога</span></RouterLink>
         </nav>
     </div>
 </template>
@@ -23,12 +23,50 @@
     width:60px;
     height:60px;
 }
+nav{
+    pointer-events: none;
+    transition: all .3s ease;
+}
 nav>*{
+    pointer-events: all;
     text-transform: uppercase;
     color: white;
     font-size: 24px;
+    text-decoration: none;
+    cursor: pointer;
+    opacity: 1;
+    line-height: 1;
 }
-nav>*:hover{
-    text-decoration: underline;
+nav:hover span{
+    opacity: .75;
 }
+nav span:hover{
+    opacity: 1;
+}
+nav>*>{
+    position: relative;
+}
+nav>*:before {
+    position: absolute; 
+    content: "";
+    display: block;
+    width: 100%;
+    height: 2px;
+    border-radius: 2px;
+    background-color: white;
+    bottom: 0;
+    left: 0;
+    transform-origin: left;
+    transform: scaleX(0);
+    transition: .3s ease-out;
+}
+/* nav>*:hover {
+    color: green;
+} */
+nav>*:hover:before {
+    transform-origin: right;
+    transform: scaleX(1);
+} 
+
+
 </style>
