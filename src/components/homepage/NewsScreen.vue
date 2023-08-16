02efd5cc5df80158aeaa9e8ab18147cc5c0af299
spineag2 <spineag@gmail.com>
2023-08-16 13:44:23 +0300

    fill news item with test info

--- src/components/homepage/NewsScreen.vue
+++ src/components/homepage/NewsScreen.vue
@@ -39,7 +39,6 @@ function chooseNewsType(nType){
                         <path stroke-linecap="round" stroke-linejoin="round" d="M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
                         <path stroke-linecap="round" stroke-linejoin="round" d="M15.91 11.672a.375.375 0 010 .656l-5.603 3.113a.375.375 0 01-.557-.328V8.887c0-.286.307-.466.557-.327l5.603 3.112z" />
                         </svg>
-
                     </button>
                     <button title="Статті" class="news_btn_icon blog_btn" @click="chooseNewsType('blog')">
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" class=" stroke-gray-400 hover:stroke-gray-700">
@@ -50,7 +49,7 @@ function chooseNewsType(nType){
             </div>
             
             <div class="news_items_container">
-                <NewsItem v-for="news of newsStore.newsByType" :key="news.id" :news="news" />
+                <NewsItem v-for="nws of newsStore.newsByType" :key="nws.id" :newsItem="nws" />
             </div>
 
             <div class="news_nav_container">
--- src/components/homepage/NewsScreenItem.vue
+++ src/components/homepage/NewsScreenItem.vue
@@ -1,46 +1,48 @@
-<!-- <script>
-import SimpleArrow from '../parts/SimpleArrow.vue';
-
-export default {
-  components: {
-    SimpleArrow
-  },
-  props: [],
-  data() {
-    return {
-        isHover: false
-    }
-  }
-}
-</script> -->
-
 <script setup>
 import SimpleArrow from '../parts/SimpleArrow.vue';
-// import useNewsStore from '../../stores/NewsStore';
+import { ref, computed } from "vue";
+// import { useNewsStore } from '../../stores/NewsStore';
 
 // const newsStore = useNewsStore();
 const isHover = false;
 const props = defineProps({
-  news: {
+  newsItem: {
     type: Object,
     required: true,
     default: () => {},
   }
 });
+
+const bg = ref('url("' + props.newsItem.preview + '")');
+
+function adaptiveText(txt){
+    if (txt.length < 120) return txt;
+    return txt.slice(0,117) + '...';
+}
+
 </script>
 
 <template>
     <div class="news_item velo_shadow" @mouseenter="isHover = true" @mouseleave="isHover = false">
-       <div class="news_item_img" style="background-image: url('src/assets/img/photo_bikers.jpg');"> </div>
+       <div class="news_item_img"> </div>
         <div class="news_item_texts">
             <div class="news_item_type bg-emerald-600">
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="inherit" class="w-6 h-6">
-                <path stroke-linecap="round" stroke-linejoin="round" d="M12 7.5h1.5m-1.5 3h1.5m-7.5 3h7.5m-7.5 3h7.5m3-9h3.375c.621 0 1.125.504 1.125 1.125V18a2.25 2.25 0 01-2.25 2.25M16.5 
-                7.5V18a2.25 2.25 0 002.25 2.25M16.5 7.5V4.875c0-.621-.504-1.125-1.125-1.125H4.125C3.504 3.75 3 4.254 3 4.875V18a2.25 2.25 0 002.25 2.25h13.5M6 7.5h3v3H6v-3z" /> </svg>
+                <svg v-if="newsItem.type=='news'" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="inherit" class="w-6 h-6">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M12 7.5h1.5m-1.5 3h1.5m-7.5 3h7.5m-7.5 3h7.5m3-9h3.375c.621 0 1.125.504 1.125 1.125V18a2.25 2.25 0 01-2.25 2.25M16.5 
+                    7.5V18a2.25 2.25 0 002.25 2.25M16.5 7.5V4.875c0-.621-.504-1.125-1.125-1.125H4.125C3.504 3.75 3 4.254 3 4.875V18a2.25 2.25 0 002.25 2.25h13.5M6 7.5h3v3H6v-3z" /> </svg>
+                <svg v-if="newsItem.type=='action'" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" class="w-6 h-6">
+                    <path stroke-linecap="round" d="M16.5 12a4.5 4.5 0 11-9 0 4.5 4.5 0 019 0zm0 0c0 1.657 1.007 3 2.25 3S21 13.657 21 12a9 9 0 10-2.636 6.364M16.5 12V8.25" /></svg>
+                <svg v-if="newsItem.type=='video'" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" class="w-6 h-6">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M15.91 11.672a.375.375 0 010 .656l-5.603 3.113a.375.375 0 01-.557-.328V8.887c0-.286.307-.466.557-.327l5.603 3.112z" />
+                    </svg>
+                <svg v-if="newsItem.type=='blog'" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" class="w-6 h-6">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M12 6.042A8.967 8.967 0 006 3.75c-1.052 0-2.062.18-3 .512v14.25A8.987 8.987 0 016 18c2.305 0 4.408.867 
+                    6 2.292m0-14.25a8.966 8.966 0 016-2.292c1.052 0 2.062.18 3 .512v14.25A8.987 8.987 0 0018 18a8.967 8.967 0 00-6 2.292m0-14.25v14.25" /></svg>
             </div>
-            <div class="news_item_date text-gray-400">12 травня</div>
-            <div class="news_item_title">Фрукти - важлива частина здорового харчування</div>
-            <div class="news_item_text">hasdgs dfhgsdfklh gsdfhgdsflj kghsdflgh sdflkghsdfklj ghsdfjklghsdfkl gjghsdfjklg hsdfkljgh kldfghdsf kljghsdfjklg</div>
+            <div class="news_item_date text-gray-400">{{ newsItem.date }}</div>
+            <div class="news_item_title">{{ newsItem.title }}</div>
+            <div class="news_item_text">{{ adaptiveText(newsItem.text) }}</div>
         </div>
         <div class="news_item_link">
             <SimpleArrow :class="{ contonhover: isHover }" />
@@ -62,6 +64,7 @@ const props = defineProps({
         background-repeat: no-repeat;
         background-position: center;
         background-size: cover;
+        background-image: v-bind(bg);
     }
     .news_item_texts{
         position: relative;
@@ -105,7 +108,7 @@ const props = defineProps({
         transform: translateY(-100px);
     }
     .news_item:hover .news_item_title{
-        font-weight: 700;
+        font-weight: 800;
     }
     .news_item:hover .news_item_text {
         opacity: 1;
