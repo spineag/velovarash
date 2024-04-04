51edad946eaedec99813806aa8fe4b7659dffbf1
andyzerovolos <andyzerovolos@google.com>
2024-04-04 21:51:41 +0300

    fix small photos hover

--- src/components/homepage/AboutPhotoGallery.vue
+++ src/components/homepage/AboutPhotoGallery.vue
@@ -13,17 +13,20 @@ let arrayUrls = ref([]);
 const shuffle = (arr) => { 
     return arr.sort(() => Math.random() - 0.5); 
 };
+let ar = shuffle(arrayAllUrls).splice(0,40);
+for (let k=0;k<20;k++){
+	arrayUrls.value.push({url1:ar[2*k], url2:ar[2*k+1]});
+}
 
 function initPics(){
     const nodes = [].slice.call(document.querySelectorAll('.layer_item_cont'), 0);
     const directions  = { 0: 'top', 1: 'right', 2: 'bottom', 3: 'left' };
     const classNames = ['in', 'out'].map((p) => Object.values(directions).map((d) => `${p}-${d}`)).reduce((a, b) => a.concat(b));
-	
 
     const getDirectionKey = (ev, node) => {
 		const { width, height, top, left } = node.getBoundingClientRect();
-		const l = ev.pageX - (left + window.pageXOffset);
-		const t = ev.pageY - (top + window.pageYOffset);
+		const l = ev.pageX - (left + window.scrollX);
+		const t = ev.pageY - (top + window.scrollY);
 		const x = (l - (width/2) * (width > height ? (height/width) : 1));
 		const y = (t - (height/2) * (height > width ? (width/height) : 1));
 		return Math.round(Math.atan2(y, x) / 1.57079633 + 5) % 4;
@@ -31,7 +34,7 @@ function initPics(){
 
 	class Item {
 		constructor(element) {
-			this.element = element;    
+			this.element = element;   
 			this.element.addEventListener('mouseover', (ev) => this.update(ev, 'in'));
 			this.element.addEventListener('mouseout', (ev) => this.update(ev, 'out'));
 		}
@@ -48,12 +51,7 @@ function initPics(){
 // const handleResize = () => {};
 onMounted(()=>{
 	// window.addEventListener('resize', handleResize);
-	let ar = shuffle(arrayAllUrls).splice(0,40);
-	for (let k=0;k<20;k++){
-		arrayUrls.value.push({url1:ar[2*k], url2:ar[2*k+1]});
-	}
-	console.log(arrayUrls.value);
-	initPics();
+		initPics();
 });
 // onUnmounted(() => {
 //     window.removeEventListener('resize', handleResize);
@@ -84,6 +82,7 @@ onMounted(()=>{
 .layer_item_cont {
 	width: 250px;
 	height: 150px;
+	cursor: cell;
 }
 .layer1 {
 	width: 100%;
